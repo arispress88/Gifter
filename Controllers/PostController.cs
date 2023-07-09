fc15c684376c5c096b55cb4487595ea09d15fa15
Arnold Rispress <arnold.rispress@gmail.com>
2023-07-09 18:49:32 -0400

    completed chap 3, 4, 5

---
+++ Controllers/PostController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Gifter.Repositories;
+using Gifter.Models;
+
+namespace Gifter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostRepository _postRepository;
+        public PostController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        // GET: /api/Post/
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_postRepository.GetAll());
+        }
+
+        // GET: /api/Post/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var post = _postRepository.GetById(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+        }
+
+
+        // POST: /api/Post/
+        [HttpPost]
+        public IActionResult Post(Post post)
+        {
+            _postRepository.Add(post);
+            return CreatedAtAction("Get", new { id = post.Id }, post);
+        }
+
+
+        // PUT: /api/Post/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Post post)
+        {
+            if (id != post.Id)
+            {
+                return BadRequest();
+            }
+
+            _postRepository.Update(post);
+            return NoContent();
+        }
+
+
+        // DELETE: /api/Post/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _postRepository.Delete(id);
+            return NoContent();
+        }
+
+
+        // GET: /api/Post/GeAllWithComments
+        [HttpGet("GetAllWithComments")]
+        public IActionResult GetAllWithComments()
+        {
+            var posts = _postRepository.GetAllWithComments();
+            return Ok(posts);
+        }
+
+
+        [HttpGet("search")]
+        public IActionResult Search(string q, bool sortDesc)
+        {
+            return Ok(_postRepository.Search(q, sortDesc));
+        }
+
+        [HttpGet("hottest")]
+        public IActionResult GetHottestPosts(DateTime since)
+        {
+            var posts = _postRepository.Search(null, true, since);
+            return Ok(posts);
+        }
+    }
+}
